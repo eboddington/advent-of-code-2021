9d7050c0139d43768d6cceb3f378e47d25d9a503
Ellis Boddington <ellis.boddington@king.com>
2021-12-03 16:39:11 +0000

    Tidying and docs

--- python/day_3.py
+++ python/day_3.py
@@ -31,7 +31,7 @@ def _get_rating(
     nth_bits = _get_nth_bits(binary_numbers)
 
     filtered_by_bit_criteria = list(
-        filter(lambda x: x[pos] == bit_criteria(nth_bits[pos + 1]), binary_numbers)
+        filter(lambda x: x[pos] == bit_criteria(nth_bits[pos]), binary_numbers)
     )
 
     if len(filtered_by_bit_criteria) == 1:
@@ -43,9 +43,24 @@ def _get_rating(
 
 
 def _get_nth_bits(binary_numbers: list) -> dict:
+    """
+    Basically rotates the data to look at columns, so turns:
+    10110
+    01001
+    11100
+
+    into:
+
+    {
+      0: [1, 0, 1],
+      1: [0, 1, 1],
+      2: [1, 0, 1]
+      etc...
+    }
+    """
     return {
-        n: [number[n - 1] for number in binary_numbers]
-        for n in range(1, len(binary_numbers[0]) + 1)
+        n: [number[n] for number in binary_numbers]
+        for n in range(len(binary_numbers[0]))
     }
 
 
